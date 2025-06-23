539f01122cc96bb5e6d557844d5831c2ca376b73
simantinip04 <149346103+simantinip04@users.noreply.github.com>
2025-06-23 17:06:48 +0530

    Update mssql_script.sql

--- Customer Order Insights & Delivery Tracker/Week1/mssql_script.sql
+++ Customer Order Insights & Delivery Tracker/Week1/mssql_script.sql
@@ -59,3 +59,23 @@ BEGIN
     FROM Orders
     WHERE CustomerID = @CustomerID AND DeliveryDate < GETDATE() AND Status != 'Delivered';
 END;
+
+ALTER TABLE Customers ADD Region VARCHAR(50);
+
+UPDATE Customers SET Region = 'North' WHERE CustomerID = 101;
+UPDATE Customers SET Region = 'South' WHERE CustomerID = 102;
+
+Select * from Customers;
+
+INSERT INTO Customers (CustomerID, Name, Email, Phone, Region)
+VALUES 
+(103, 'Raj Verma', 'raj.verma@example.com', '9988776655', 'West'),
+(104, 'Sneha Reddy', 'sneha.reddy@example.com', '9871234567', 'South'),
+(105, 'Manoj Das', 'manoj.das@example.com', '9123987654', 'East'),
+(106, 'Neha Kulkarni', 'neha.kulkarni@example.com', '7894561230', 'West'),
+(107, 'Karan Mehta', 'karan.mehta@example.com', '7001239876', 'North'),
+(108, 'Divya Iyer', 'divya.iyer@example.com', '8080808080', 'South'),
+(109, 'Arjun Sen', 'arjun.sen@example.com', '9900990099', 'East'),
+(110, 'Meera Joshi', 'meera.joshi@example.com', '8500450060', 'West');
+
+Select * from Customers;
